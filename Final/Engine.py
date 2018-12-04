05c4cbd282980a040f5d9a31757edab336f23545
Wilfredo J. Molina <molina.wilfredo.j@gmail.com>
2018-12-04 16:03:21 -0600

    bug fixes

--- Final/Engine.py
+++ Final/Engine.py
@@ -3,7 +3,7 @@ import os
 from Entity import Entity
 from Creature import Creature
 from random import randint, random
-from math import sqrt, ceil
+from math import sqrt
 from numpy import array
 
 
@@ -29,13 +29,15 @@ class Engine(object):
             self.entities.append(Entity(x = randint(0, self.world_width), y = randint(0, self.world_height), kind = 'cheese'))
 
         if file_name is None:
+            print('no file given: creating new file')
             for i in range(0, n_mice):
                 self.entities.append(Creature(x = randint(0, self.world_width), y = randint(0, self.world_height), kind = 'mouse', brain_shape = self.brain_shape))
 
             for i in range(0, n_cats):
                 self.entities.append(Creature(x = randint(0, self.world_width), y = randint(0, self.world_height), kind = 'cat', brain_shape = self.brain_shape))
         else:
-            file = open(file_name, "r")
+            print('loading file')
+            file = open(file_name, 'r')
             lines = file.readlines()
             file.close()
 
@@ -58,48 +60,42 @@ class Engine(object):
 
         return fittest
 
+    def sort_generation(self):
+        sorted_generations = { }
+
+        for entity in self.entities:
+            if entity.kind != 'cheese':
+                if entity.kind in sorted_generations:
+                    sorted_generations[entity.kind].append(entity)
+                else:
+                    sorted_generations[entity.kind] = []
+
+        for creature in sorted_generations:
+            sorted_generations[creature].sort(key = lambda x: x.fitness, reverse = True)
+
+        return sorted_generations
+
     def tick(self):
         if self.generation_time == self.generation_life:
-            mice = []
+            self.generation_time = 0
 
-            for entity in self.entities:
-                if entity.kind == 'mouse':
-                    mice.append(entity)
-
-            cats = []
-
-            for entity in self.entities:
-                if entity.kind == 'cat':
-                    cats.append(entity)
-
-            # Elitism
-
-            best_mouse = self.get_fittest('mouse')
-            self.entities.remove(best_mouse)
-            second_best_mouse = self.get_fittest('mouse')
-
-            best_cat = self.get_fittest('cat')
-            self.entities.remove(best_cat)
-            second_best_cat = self.get_fittest('cat')
-
-            self.entities = [best_mouse, second_best_mouse, best_cat, second_best_cat]
+            sorted_generations = self.sort_generation()
+            self.entities = [sorted_generations['mouse'][0], sorted_generations['mouse'][1], sorted_generations['cat'][0], sorted_generations['cat'][1]]
 
             for i in range(0, self.n_mice - 2):
-                self.entities.append(self.create_child(self.roulette_wheel(mice), self.roulette_wheel(mice), 'mouse'))
+                self.entities.append(self.create_child(self.roulette_wheel(sorted_generations['mouse']), self.roulette_wheel(sorted_generations['mouse'])))
 
             for i in range(0, self.n_cheese):
                 self.entities.append(Entity(x = randint(0, self.world_width), y = randint(0, self.world_height), kind = 'cheese'))
 
             for i in range(0, self.n_cats - 2):
-                self.entities.append(self.create_child(self.roulette_wheel(cats), self.roulette_wheel(cats), 'cat'))
-
-            self.generation_time = 0
+                self.entities.append(self.create_child(self.roulette_wheel(sorted_generations['cat']), self.roulette_wheel(sorted_generations['cat'])))
 
             for entity in self.entities:
                 if entity.kind == 'mouse' or entity.kind == 'cat':
                     self.assign_prey_and_predator(entity)
 
-            self.save_generation("mice_generation")
+            self.save_generation('mice_generation')
 
             return False
         else:
@@ -135,37 +131,37 @@ class Engine(object):
             if wheel > ball:
                 return candidate
 
-    def create_child(self, dominant_gene, recessive_gene, kind):
-        child = Creature(x = randint(0, self.world_width), y = randint(0, self.world_height), kind = kind, brain_shape = self.brain_shape)
+    def create_child(self, parent_1, parent_2):
+        child = Creature(x = randint(0, self.world_width), y = randint(0, self.world_height), kind = parent_1.kind, brain_shape = self.brain_shape)
 
         # Cross-Over
+
         for key in child.brain.W:
             for i in range(0, len(child.brain.W[key])):
                 for j in range(0, len(child.brain.W[key][i])):
                     if random() < self.cross_over_rate:
-                        child.brain.W[key][i, j] = dominant_gene.brain.W[key][i, j]
+                        child.brain.W[key][i, j] = parent_1.brain.W[key][i, j]
                     else:
-                        child.brain.W[key][i, j] = recessive_gene.brain.W[key][i, j]
+                        child.brain.W[key][i, j] = parent_2.brain.W[key][i, j]
+
         for key in child.brain.b:
             for i in range(0, len(child.brain.b[key])):
                 if random() < self.cross_over_rate:
-                    child.brain.b[key][i] = dominant_gene.brain.b[key][i]
+                    child.brain.b[key][i] = parent_1.brain.b[key][i]
                 else:
-                    child.brain.b[key][i] = recessive_gene.brain.b[key][i]
+                    child.brain.b[key][i] = parent_2.brain.b[key][i]
 
         # Mutation
+
         for key in child.brain.W:
             for i in range(0, len(child.brain.W[key])):
                 for j in range(0, len(child.brain.W[key][i])):
                     if random() < self.mutation_rate:
                         child.brain.W[key][i, j] = (child.brain.B - child.brain.A) * random() + child.brain.A
-                    else:
-                        child.brain.W[key][i, j] = (child.brain.B - child.brain.A) * random() + child.brain.A
+
         for key in child.brain.b:
             for i in range(0, len(child.brain.b[key])):
                 if random() < self.mutation_rate:
-                    child.brain.b[key][i] = (child.brain.B - child.brain.A) * random() + child.brain.A
-                else:
                     child.brain.b[key][i] = (child.brain.B - child.brain.A) * random() + child.brain.A
 
         return child
@@ -201,7 +197,7 @@ class Engine(object):
         creature.target_prey_and_predator(prey, predator)
 
     def save_generation(self, file_name):
-        file = open(file_name + '_backup.txt', "w")
+        file = open(file_name + '_backup.txt', 'w')
         lines = []
 
         for entity in self.entities:
--- Final/main.py
+++ Final/main.py
@@ -2,12 +2,12 @@ from Engine import Engine
 from GUI import GUI
 from math import inf
 
-GUI(Engine(world_width = 600,
-           world_height = 600,
-           n_mice = 10,
-           n_cheese = 20,
+GUI(Engine(world_width = 1000,
+           world_height = 1000,
+           n_mice = 20,
+           n_cheese = 30,
            n_cats = 10,
            n_generations = inf,
-           cross_over_rate = .75,
+           cross_over_rate = .5,
            mutation_rate = .01,
            file_name = 'mice_generation.txt'))
