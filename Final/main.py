90c850618762cf07f6b5a3bd961a428b570d86d8
Wilfredo J. Molina <molina.wilfredo.j@gmail.com>
2018-12-09 18:20:44 -0600

    hyper parameters change

--- Final/main.py
+++ Final/main.py
@@ -5,11 +5,11 @@ from math import inf
 GUI(Engine(world_width = 500,
            world_height = 500,
            n_mice = 10,
-           n_cheese = 15,
+           n_cheese = 5,
            n_cats = 0,
            n_generations = inf,
            generation_life = 5000,
            cross_over_rate = .5,
-           mutation_rate = .05,
+           mutation_rate = .01,
            chromosomes_file = 'chromosomes_file',
            stats_file = 'stats_file'), show = False)
